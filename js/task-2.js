d36d6f78701e1cd7e08e4af0e69f839c180b25c5
venuscorpua <venuscorpua@gmail.com>
2024-05-30 14:47:22 +0300

    upd

--- js/task-2.js
+++ js/task-2.js
@@ -1,16 +1,36 @@
-function makeArray(firstArray, secondArray, maxLength){
-    arrayNew = firstArray.concat(secondArray);
-    if (arrayNew.length >= maxLength) {
-        return arrayNew.slice(0,maxLength)
-    }
-    else{
-        return arrayNew
-    }
+function calcAverageCalories(days) {
+  let sumCalories = 0;
+  const calories = Object.values(days);
+  let indexDay = calories.length;
+
+  for (const key of calories) {
+    sumCalories = sumCalories + key.calories;
+  }
+  return indexDay > 0 ? sumCalories / indexDay : 0;
 }
 
-console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
-console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
-console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 3010 },
+    { day: "tuesday", calories: 3200 },
+    { day: "wednesday", calories: 3120 },
+    { day: "thursday", calories: 2900 },
+    { day: "friday", calories: 3450 },
+    { day: "saturday", calories: 3280 },
+    { day: "sunday", calories: 3300 },
+  ])
+); // 3180
+
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 2040 },
+    { day: "tuesday", calories: 2270 },
+    { day: "wednesday", calories: 2420 },
+    { day: "thursday", calories: 1900 },
+    { day: "friday", calories: 2370 },
+    { day: "saturday", calories: 2280 },
+    { day: "sunday", calories: 2610 },
+  ])
+); // 2270
+
+console.log(calcAverageCalories([])); // 0
